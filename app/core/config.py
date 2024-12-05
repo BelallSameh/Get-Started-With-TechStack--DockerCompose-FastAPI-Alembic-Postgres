f6a0d6504637c9a7fc0d369a86218a356cdfea45
BelallSameh <belalsameh188@gmail.com>
2024-12-05 15:10:20 +0200

    Update files from local directory

--- app/core/config.py
+++ app/core/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 class Settings:
     DATABASE_URL: str = os.getenv("DATABASE_URL","postgresql://youruser:yourpassword@db:5432/yourdb")
     GEMINI_API_KEY: str = os.environ.get("GEMINI_API_KEY","AIzaSyCeECOknuR_tIhqysYalCnO4TECzDYo9tw")
-    GEMINI_MODEL =  'gemini-1.5-pro'
+    GEMINI_MODEL =  'gemini-1.0-pro'
 settings = Settings()
